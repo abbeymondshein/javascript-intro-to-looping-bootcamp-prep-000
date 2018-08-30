a67abe12ea829f15a8cbab68f1e32e25688dcdfa
Abbey Mondshein <abbey.sync@gmail.com>
2018-08-30 22:41:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,3 +6,5 @@
 //condition: An expression evaluated before each loop iteration. If this expression evaluates to true, statement is executed
 //iteration: A statement executed at the end of each iteration. Typically, this will involve incrementing or decrementing a counter, bringing the loop ever closer to its end
 //loopBody: Code which runs on every iteration as long as the condition evaluates to true
+//Use a for loop when you know how many times you want the loop to run (for example, when you have an array of known size).
+
