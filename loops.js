19a32ae16ff5ad3ee472e3353a6cf3183d3c59d8
Abbey Mondshein <abbey.sync@gmail.com>
2018-08-30 22:40:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,4 +3,5 @@
 //}
 
 //initialization: An expression (including assignment expressions) or variable declaration. Typically used to initialize a counter variable. This expression may optionally declare new variables with the var keyword
-//condition: 
+//condition: An expression evaluated before each loop iteration. If this expression evaluates to true, statement is executed
+//iteration: 
